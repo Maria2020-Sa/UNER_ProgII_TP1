9f4a69475e4c40383737c5fc9feae30606af3eaf
Maria2020-Sa <maugesara@gmail.com>
2024-08-20 15:25:05 -0300

    A sentence is added that validates that age

--- seccion_b/refactorizar.py
+++ seccion_b/refactorizar.py
@@ -1,13 +1,13 @@
 import time
 from calendar import isleap
 
-# a) Se elimine la sentencia if / else de la función anio_bisiesto.
+# a. Se elimine la sentencia if / else de la función anio_bisiesto.
 def anio_bisiesto(anio):
     #if isleap(anio): return True
     #else: return False
     return isleap(anio)
 
-# calcular el numero de dias de cada mes
+# b. Las múltiples sentencias if la función dia_mes utilicen la cláusula in en lugar de varias cláusulas or
 def calcular_dias_mes(mes, anio_bisiesto):
     if mes == 1 or mes == 3 or mes == 5 or mes == 7 or mes == 8 or mes == 10 or mes == 12:
         return 31
@@ -17,25 +17,44 @@ def calcular_dias_mes(mes, anio_bisiesto):
         return 29
     elif mes == 2 and anio_bisiesto == False:
         return 28
+    
 # ingreso de datos del usuario
 nombre = input("Ingrese su nombre: ")
 edad = input("Ingrese su edad: ")
+
+#c. Se agregue una sentencia que valide que la edad ingresada por el usuario es numérica.
+def validar_edad (edad):
+    try:
+        edad = int(edad)
+        if edad < 0:
+            print("La edad no puede ser un valor negativo.")
+            return False
+        else:
+            return edad
+    except ValueError:
+        print("Error: La edad debe ser un número.")
+        return False
+
 # seteo inicial de variables
 hora_local = time.localtime(time.time())
-anios = int(edad)
+anios = validar_edad(edad)
 anio_comienzo = int(hora_local.tm_year) - anios
 anio_fin = anio_comienzo + anios
 meses = anios * 12 + hora_local.tm_mon
 dias = 0
+
 # calcular los dias
 for a in range(anio_comienzo, anio_fin):
     if (anio_bisiesto(a)): dias = dias + 366
 else: dias = dias + 365
+
 # agregar los días transcurridos en este año
 for m in range(1, hora_local.tm_mon):
     dias = dias + calcular_dias_mes(m, anio_bisiesto(hora_local.tm_year))
+
 # agregar los días transcurridos en este mes
 dias = dias + hora_local.tm_mday
+
 # imprimir la edad del usuario
 print("La edad de %s es %d años o " % (nombre, anios), end="")
 print("%d meses o %d días" % (meses, dias))
